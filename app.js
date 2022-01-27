de83b0eaec84cd4be0e8186f874822f7748a13ad
MiguelAOCosta <miguelangelocosta1996@gmail.com>
2022-01-27 01:55:26 +0000

    Correction folder name

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const routes = require("./routes/apiRoute");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/api", routes);
+app.use("/api", require("./Routes/apiRoute"));
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("Example app listening at http://localhost:3001");
